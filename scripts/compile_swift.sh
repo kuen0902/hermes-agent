85f8108fcbcaa371a7e1dc6ba0f1cdf0bc28ba48
Kuen Tsai <kuen0902@gmail.com>
2026-06-27 22:16:45 +0800

    Optimize architecture according to implementation plan

--- scripts/compile_swift.sh
+++ scripts/compile_swift.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 cd /Users/bookid/.hermes/scripts
+
+mkdir -p bin
+
 echo "編譯 hermes_orchestrator..."
-swiftc -O hermes_orchestrator.swift -o hermes_orchestrator
+swiftc -O hermes_orchestrator.swift -o bin/hermes_orchestrator
 
 echo "編譯 hermes_sync..."
-swiftc -O hermes_sync.swift -o hermes_sync
+swiftc -O hermes_sync.swift -o bin/hermes_sync
 
 echo "編譯 hermes_monitor..."
-swiftc -O hermes_monitor.swift -o hermes_monitor
+swiftc -O hermes_monitor.swift -o bin/hermes_monitor
 
 echo "編譯完成！"
